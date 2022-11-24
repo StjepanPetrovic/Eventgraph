e9a3c65c75b76cfadb49644881d7c96971b6ce4f
StjepanPetrovic <stjepanpetrovic11@gmail.com>
2022-11-24 16:43:10 +0100

    Install the Places SDK for iOS and add API key

--- Eventgraph/AppDelegate.swift
+++ Eventgraph/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import GoogleMaps
+import GooglePlaces
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         GMSServices.provideAPIKey("AIzaSyDyGfqbBc8rtRwCbCym-4kQHuT6dOLfBIo")
+        GMSPlacesClient.provideAPIKey("AIzaSyDyGfqbBc8rtRwCbCym-4kQHuT6dOLfBIo")
         return true
     }
 

0290fb0df6e6ef7eaf90597408f022f7c6a8bc14
StjepanPetrovic <stjepanpetrovic11@gmail.com>
2022-11-24 18:54:32 +0100

    Set up the location manager

--- Eventgraph/MapViewController.swift
+++ Eventgraph/MapViewController.swift
@@ -22,11 +22,36 @@
 
 import UIKit
 import GoogleMaps
+import GooglePlaces
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager: CLLocationManager!
+    var currentLocation: CLLocation?
+    var mapView: GMSMapView!
+    var placesClient: GMSPlacesClient!
+    var preciseLocationZoomLevel: Float = 15.0
+    var approximateLocationZoomLevel: Float = 10.0
+    
+    // An array to hold the list of likely places.
+    var likelyPlaces: [GMSPlace] = []
+
+    // The currently selected place.
+    var selectedPlace: GMSPlace?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Initialize the location manager.
+        locationManager = CLLocationManager()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.distanceFilter = 50
+        locationManager.startUpdatingLocation()
+        locationManager.delegate = self
+
+        placesClient = GMSPlacesClient.shared()
+        
         // Do any additional setup after loading the view.
         // Create a GMSCameraPosition that tells the map to display the
         // coordinate -33.86,151.20 at zoom level 6.
@@ -41,4 +66,91 @@ class MapViewController: UIViewController {
         marker.snippet = "Australia"
         marker.map = mapView
     }
+
+    // Populate the array with the list of likely places.
+    func listLikelyPlaces() {
+        // Clean up from previous sessions.
+        likelyPlaces.removeAll()
+
+        let placeFields: GMSPlaceField = [.name, .coordinate]
+        placesClient.findPlaceLikelihoodsFromCurrentLocation(withPlaceFields: placeFields) { (placeLikelihoods, error) in
+          guard error == nil else {
+              // TODO: Handle the error.
+              print("Current Place error: \(error!.localizedDescription)")
+              return
+            }
+
+            guard let placeLikelihoods = placeLikelihoods else {
+                print("No places found.")
+                return
+            }
+
+            // Get likely places and add to the list.
+            for likelihood in placeLikelihoods {
+                let place = likelihood.place
+                self.likelyPlaces.append(place)
+            }
+        }
+    }
+}
+
+// Delegates to handle events for the location manager.
+extension MapViewController {
+
+  // Handle incoming location events.
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    let location: CLLocation = locations.last!
+    print("Location: \(location)")
+
+    let zoomLevel = locationManager.accuracyAuthorization == .fullAccuracy ? preciseLocationZoomLevel : approximateLocationZoomLevel
+    let camera = GMSCameraPosition.camera(withLatitude: location.coordinate.latitude,
+                                          longitude: location.coordinate.longitude,
+                                          zoom: zoomLevel)
+
+    if mapView.isHidden {
+      mapView.isHidden = false
+      mapView.camera = camera
+    } else {
+      mapView.animate(to: camera)
+    }
+
+    listLikelyPlaces()
+  }
+
+  // Handle authorization for the location manager.
+  func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+    // Check accuracy authorization
+    let accuracy = manager.accuracyAuthorization
+    switch accuracy {
+    case .fullAccuracy:
+        print("Location accuracy is precise.")
+    case .reducedAccuracy:
+        print("Location accuracy is not precise.")
+    @unknown default:
+      fatalError()
+    }
+
+    // Handle authorization status
+    switch status {
+    case .restricted:
+      print("Location access was restricted.")
+    case .denied:
+      print("User denied access to location.")
+      // Display the map using the default location.
+      mapView.isHidden = false
+    case .notDetermined:
+      print("Location status not determined.")
+    case .authorizedAlways: fallthrough
+    case .authorizedWhenInUse:
+      print("Location status is OK.")
+    @unknown default:
+      fatalError()
+    }
+  }
+
+  // Handle location manager errors.
+  func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+    locationManager.stopUpdatingLocation()
+    print("Error: \(error)")
+  }
 }
